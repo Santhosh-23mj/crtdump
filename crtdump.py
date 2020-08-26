b266e1f1486c930e7d81c9ea12f6958752c369ef
Santhosh-23mj <33282970+Santhosh-23mj@users.noreply.github.com>
2020-08-26 12:20:50 +0000

    Create crtdump.py

---
+++ crtdump.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+
+import mechanize
+from bs4 import BeautifulSoup
+
+# Function to get the HTML Document
+
+def getPage(url):
+	browser = mechanize.Browser()
+	browser.set_handle_robots(False)
+	webPage = browser.open(url)
+	return webPage
+
+# Function to parse the WebPage for domains and subdomains
+
+def findData(webPage,domain):
+	links 	    = []
+	tempdomains = []
+	subdomains  = []
+	soup        = BeautifulSoup(webPage, 'html.parser')
+	
+	# Fetch all the elements with <td> tag
+	for link in soup.find_all("table")[-1].findAll('td'):
+		links.append(link)
+	
+	# Fetch all the elements that contain the data about the domain
+	for text in links:
+		if(domain in text.find(text=True)):
+			tempdomains.append(text)
+
+	# Create a list of all subdomains
+	for subdomain in tempdomains:
+		subdomains.append(subdomain.findAll(text=True))
+	
+	return subdomains
+
+# Function to sort the list of lists to a single list with unique entries
+
+def sortOutData(subdomains):
+	domains = []
+	uniqueNames = []
+	for hosts in subdomains:
+		for host in hosts:
+			domains.append(host)
+	uniqueNames = list(set(domains))
+	return uniqueNames
+
+# Function to print out the subdomains
+
+def printResult(domains,domain):
+    for host in domains:
+        print(host)
+    print()
+
+# Main Function
+
+def main():
+	recon      = input("Enter the domain to lookup : ")
+	url        = "https://crt.sh/?q="+recon
+	webPage    = getPage(url)
+	subdomains = findData(webPage, recon)
+	result     = sortOutData(subdomains)
+	printResult(result)
+
+if(__name__ == '__main__'):
+	main()
